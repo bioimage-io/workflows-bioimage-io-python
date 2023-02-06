77306bf14ff942d344cc2047758798a6503521e7
fynnbe <thefynnbe@gmail.com>
2023-02-06 11:17:17 +0100

    clean up example.py

--- scripts/example.py
+++ scripts/example.py
@@ -3,22 +3,21 @@ import asyncio
 from bioimageio.core import load_resource_description
 from bioimageio.core.image_helper import load_image
 from bioimageio.core.resource_io.nodes import Model
-from bioimageio.spec.shared import resolve_source
-from bioimageio.workflows import stardist_prediction_2d, hello
+from bioimageio.workflows import hello, stardist_prediction_2d
 
 
 async def main():
     print(await hello())
 
     rdf = "chatty-frog"
-    model = load_resource_description("chatty-frog")
-    assert isinstance(model, Model)
-    # expected_labels = load_image(
-    #     resolve_source("https://zenodo.org/record/7372477/files/stardist_chatty_frog_labels.npy"), tuple("byx")
-    # )
 
+    # load test input from model
+    model = load_resource_description(rdf)
+    assert isinstance(model, Model)
     raw = load_image(model.test_inputs[0], model.inputs[0].axes)
-    labels, polys = await stardist_prediction_2d(rdf, raw, tiles=None)
+
+    # call env specific workflow
+    labels, polys = await stardist_prediction_2d(rdf, raw)
 
     print("labels", labels.shape)
 
