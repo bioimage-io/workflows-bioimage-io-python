40cbef68b21308b03d9100d3b80a46adc887f1ea
fynnbe <thefynnbe@gmail.com>
2023-03-03 13:12:34 +0100

    fix test_run_model_inference_with_dask

--- tests/test_default/test_inference.py
+++ tests/test_default/test_inference.py
@@ -8,7 +8,7 @@ from bioimageio.spec.model.raw_nodes import Model as RawModel
 
 
 @pytest.mark.asyncio
-def test_run_model_inference_with_dask():
+async def test_run_model_inference_with_dask():
     from bioimageio.workflows.envs.default import inference_with_dask
 
     model = load_raw_resource_description(
@@ -19,11 +19,11 @@ def test_run_model_inference_with_dask():
     test_inputs = [load_image(p, s.axes) for p, s in zip(model.test_inputs, model.inputs)]
     expected_outputs = [load_image(p, s.axes) for p, s in zip(model.test_outputs, model.outputs)]
     outputs = await inference_with_dask(
-        model, *test_inputs, tiles=[dict(zip(ipt.axes, ipt.shape.min)) for ipt in model.inputs]
+        model, test_inputs, tiles=[dict(zip(ipt.axes, ipt.shape.min)) for ipt in model.inputs]
     )
     # todo: adapt for multiple outputs
     halo = tuple(np.s_[h:-h] if h else np.s_[:] for h in model.outputs[0].halo)
     # use for debugging:
-    outputs = [outputs[0].data.compute(scheduler="single-threaded")]
+    outputs = [outputs[model.outputs[0].name].data.compute(scheduler="single-threaded")]
     for exp, act in zip(expected_outputs, outputs):
         assert_array_almost_equal(exp[halo], act[halo])
