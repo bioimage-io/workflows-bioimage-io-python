6ccd67f89df732507bf6e973d4d123f98d7d3442
fynnbe <thefynnbe@gmail.com>
2023-03-16 14:08:22 +0100

    simplify hello demo

--- src/bioimageio/workflows/envs/default/_demo.py
+++ src/bioimageio/workflows/envs/default/_demo.py
@@ -3,26 +3,25 @@ from typing import Optional
 import xarray as xr
 
 
-async def hello(
-    msg: str = "Hello!", tensor_a: Optional[xr.DataArray] = None, tensor_b: Optional[xr.DataArray] = None
-) -> str:
+async def hello(msg: str = "Hello!", tensor: Optional[xr.DataArray] = None) -> str:
     """dummy workflow printing msg
 
     This dummy workflow is intended as a demonstration and for testing.
 
     .. code-block:: yaml
+    authors: [{name: Fynn Beuttenmüller, github_user: fynnbe, affiliation: EMBL Heidelberg}]
     cite: [{text: BioImage.IO, url: "https://doi.org/10.1101/2022.06.07.495102"}]
+    tags: [demo]
+    covers: ["https://github.com/bioimage-io/bioimage.io/raw/10db0410b15684cdeac19d795b0edb330a3a7b80/public/static/img/bioimage-io-icon.png"]
 
     Args:
         msg: Message
-        tensor_a: tensor_a whose shape is added to message
-            axes: unknown
-        tensor_b: tensor_b whose shape is added to message
+        tensor: tensor whose shape is added to message
             axes:
             - type: batch
             - type: space
               name: x
-              description: x dimension
+              description: demo space x
               unit: millimeter
               step: 1.5
             - type: index
@@ -30,13 +29,10 @@ async def hello(
               description: a special index axis
 
     Returns:
-        msg. A possibly manipulated message.
+        msg. A message, optionally enriched with the tensor shape.
     """
-    if tensor_a is not None:
-        msg += f" tensor_a shape: {tensor_a.shape}"
-
-    if tensor_b is not None:
-        msg += f" tensor_b shape: {tensor_b.shape}"
+    if tensor is not None:
+        msg += f" The tensor shape is {tensor.shape}"
 
     print(msg)
     return msg
